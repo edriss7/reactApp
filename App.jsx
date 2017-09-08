dacdff9e1b953a8791a99cd748136d01ca746848
Edris Tabandeh <Edris.Tabandeh@aexp.com>
2017-09-08 02:39:28 -0700

    got to the router stuff

--- App.jsx
+++ App.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router-dom'
 
 class App extends React.Component {
    render() {
       return (
          <div>
-            <h1>Header</h1>
-            {//End of the line Comment...
-            }
-            {/*Multi line comment...*/}
+            <ul>
+               <li><Link to = "/home">Home</Link></li>
+               <li><Link to = "/about">About</Link></li>
+               <li><Link to = "/contact">Contact</Link></li>
+            </ul>
+            
+           {this.props.children}
          </div>
-      );
+      )
    }
 }
-
 export default App;
+
+
+export class Contact extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Contact...</h1>
+         </div>
+      )
+   }
+}
--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import Home from './Home.jsx';
+import Contact from './Contact.jsx';
+import About from './About.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+import {HashRouter, Route, Router, IndexRoute} from 'react-router-dom';
+
+ReactDOM.render((
+   <HashRouter>
+      <switch>
+         <Route path = "/" component = {App} />
+         <Route path = "/home" component = {Home} />
+         <Route path = "/about" component = {About} />
+         <Route path = "/contact" component = {Contact} />
+      </switch>
+   </HashRouter>
+	
+), document.getElementById('app'))
